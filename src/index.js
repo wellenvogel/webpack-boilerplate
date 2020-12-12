00ca51504e1d46e1fad94b6737875126512d036b
andreas <andreas@wellenvogel.de>
2020-12-12 17:39:12 +0100

    simple handlebar use

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import css from '../style/style.css';
 import other from '../style/style.less'
+import HandleBars from 'handlebars';
 console.log("Hello");
 
+let template='<div class="main1"><span class="text">{{string1}}</span></div>';
+
 window.onload=()=>{
+    let compiledTemplate=HandleBars.compile(template);
     let e=document.getElementById("main");
-    if (e) e.textContent="App started";
+    if (e) e.innerHTML=compiledTemplate({string1:'HandleBars App'});
 }
