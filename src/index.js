b51c63428ebbbf06ec85e63a3f82f9e9e8703567
andreas <andreas@wellenvogel.de>
2020-12-12 17:27:14 +0100

    add react

--- src/index.js
+++ src/index.js
@@ -2,7 +2,18 @@ import css from '../style/style.css';
 import other from '../style/style.less'
 console.log("Hello");
 
+import React, { useState } from "react";
+import { render } from "react-dom";
+
+function App() {
+    const [state, setState] = useState(0);
+    const text={
+        0:'Off',
+        1:'On'
+    }
+    return <button onClick={() => setState(state===0?1:0)}>{text[state]}</button>;
+}
+
 window.onload=()=>{
-    let e=document.getElementById("main");
-    if (e) e.textContent="App started";
+    render(<App />, document.getElementById("main"));
 }
